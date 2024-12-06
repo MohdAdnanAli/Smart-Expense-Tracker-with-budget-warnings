9db54848a554e4120329450e80b5b0792c59c8a8
MohdAdnanAli <adnanali.aa@gmail.com>
2024-12-06 18:19:11 +0530

    refactor naming to the directory

--- expensetracker/src/main/java/expensetracker/expenseapp/ExpenseTrackerApplication.java
+++ expensetracker/src/main/java/expensetracker/expenseapp/ExpenseTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExpenseappApplication {
+public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExpenseappApplication.class, args);
+		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
 }
